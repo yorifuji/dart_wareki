dcffaf95de26adb9062a6be5920f75446e2e0ba4
yorifuji <yorifuji@gmail.com>
2022-06-12 00:57:01 +0900

    fix

--- lib/japanese_era.dart
+++ lib/japanese_era.dart
@@ -1,37 +1,35 @@
 String wareki(int year, int month, int day) {
-  String wareki, index;
-
+  String name, index;
   if (year > 2019) {
-    wareki = "令和";
+    name = "令和";
     index = '${year - 2018}年';
   } else if (year == 2019 && month >= 5) {
-    wareki = "令和";
+    name = "令和";
     index = "元年";
   } else if (year > 1989) {
-    wareki = "平成";
+    name = "平成";
     index = '${year - 1988}年';
   } else if (year == 1989 && (month > 1 || (month == 1 && day >= 8))) {
-    wareki = "平成";
+    name = "平成";
     index = "元年";
   } else if (year > 1926) {
-    wareki = "昭和";
+    name = "昭和";
     index = '${year - 1925}年';
   } else if (year == 1926 && (month == 12 && day >= 25)) {
-    wareki = "昭和";
+    name = "昭和";
     index = "元年";
   } else if (year > 1912) {
-    wareki = "大正";
+    name = "大正";
     index = '${year - 1911}年';
   } else if (year == 1912 && (month > 7 || (month == 7 && day >= 30))) {
-    wareki = "大正";
+    name = "大正";
     index = "元年";
   } else if (year > 1868) {
-    wareki = "明治";
+    name = "明治";
     index = '${year - 1867}年';
   } else {
-    wareki = "明治";
+    name = "明治";
     index = "元年";
   }
-
-  return wareki + index;
+  return name + index;
 }
--- test/japanese_era_test.dart
+++ test/japanese_era_test.dart
@@ -4,17 +4,20 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('Test', () {
     test('西暦 -> 和暦', () {
-      expect('令和2年', wareki(2020, 1, 1));
-      expect('令和元年', wareki(2019, 5, 2));
-      expect('令和元年', wareki(2019, 5, 1));
-      expect('平成31年', wareki(2019, 4, 30));
-      expect('平成元年', wareki(1989, 1, 8));
-      expect('昭和64年', wareki(1989, 1, 7));
-      expect('昭和元年', wareki(1926, 12, 25));
-      expect('大正15年', wareki(1926, 12, 24));
-      expect('大正元年', wareki(1912, 7, 30));
-      expect('明治45年', wareki(1912, 7, 29));
-      expect('明治元年', wareki(1868, 1, 25));
+      expect(wareki(2020, 1, 1), '令和2年');
+      expect(wareki(2019, 5, 1), '令和元年');
+      expect(wareki(2019, 4, 30), '平成31年');
+      expect(wareki(1990, 1, 1), '平成2年');
+      expect(wareki(1989, 1, 8), '平成元年');
+      expect(wareki(1989, 1, 7), '昭和64年');
+      expect(wareki(1927, 1, 1), '昭和2年');
+      expect(wareki(1926, 12, 25), '昭和元年');
+      expect(wareki(1926, 12, 24), '大正15年');
+      expect(wareki(1913, 1, 1), '大正2年');
+      expect(wareki(1912, 7, 30), '大正元年');
+      expect(wareki(1912, 7, 29), '明治45年');
+      expect(wareki(1869, 1, 1), '明治2年');
+      expect(wareki(1868, 1, 25), '明治元年');
     });
   });
 }
